c10ba1f97f0387b9f408aaf5b1a0286ac7bba8f9
gambl01d <46679106+gambl01d@users.noreply.github.com>
2022-01-02 09:37:19 -0500

    Heroku Settings
    Heroku Settings

---
+++ Phil/prod_settings.py
@@ -0,0 +1,13 @@
+""" Production Settings """
+# default: use settings from main settings.py if not overwritten
+from .settings import *
+
+import django_heroku
+
+
+DEBUG = False
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+ALLOWED_HOSTS = ['phil-dan.herokuapp.com']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
